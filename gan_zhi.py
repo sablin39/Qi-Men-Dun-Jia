284510f664d696e8d8f8e44b699c5d33c69342a8
Anastasia-Sablina <1020030829@qq.com>
2023-01-21 17:11:43 +0800

    a few modification

--- gan_zhi.py
+++ gan_zhi.py
@@ -1,5 +1,4 @@
 #-*- coding: UTF-8-*-
-import numpy as np
 from datetime import datetime
 import datetime as Datetime
 from zhdate import ZhDate
@@ -29,7 +28,7 @@ def get_jieqi(year,jieqi=None):
     else:
         return match[jieqi]
 
-def convert_date(input_date:datetime):
+def convert_date(input_date:datetime,output_type="char"):
     global tian_gan
     global di_zhi
     year,month,date=ZhDate.from_datetime(input_date).lunar_year,\
@@ -41,22 +40,33 @@ def convert_date(input_date:datetime):
     #lichun=ZhDate.from_datetime(lichun)
     if input_date<lichun:
         year-=1
-    year_res=tian_gan[(year-4)%10]+di_zhi[(year-4)%12]
+    
 
     month_tmp=((year-4)*12+2+month)%60
-    month_res=tian_gan[month_tmp%10-1]+di_zhi[month_tmp%12-1]
+    
    
     cent=input_date.year%100-1
     month_base=[0,31,-1,30,0,31,1,32,3,33,4,34]
     day_tmp=cent//4*6+5*(cent//4*3+cent%4)+month_base[input_date.month-1]+input_date.day
     if (year%4==0 and year%100!=0) or (year%400==0):
         day_tmp+=1
-    day_res=tian_gan[day_tmp%10-1]+di_zhi[day_tmp%12-1]
+    
     
     hour=input_date.hour
     hour_tmp1=round((hour / 2) + 0.1) % 12
     hour_tmp2=(day_tmp%10*2+hour_tmp1-2)%10
-    hour_res=tian_gan[hour_tmp2]+di_zhi[hour_tmp1]
+
+    if output_type=="char":
+        year_res=tian_gan[(year-4)%10]+di_zhi[(year-4)%12]
+        month_res=tian_gan[(month_tmp%10-1)%10]+di_zhi[(month_tmp%12-1)%12]
+        day_res=tian_gan[(day_tmp%10-1)%10]+di_zhi[(day_tmp%12-1)%12]
+        hour_res=tian_gan[hour_tmp2]+di_zhi[hour_tmp1]
+    else:
+        year_res=[(year-4)%10,(year-4)%12]        
+        month_res=[(month_tmp%10-1)%10,(month_tmp%12-1)%12]
+        day_res=[(day_tmp%10-1)%10,(day_tmp%12-1)%12]
+        hour_res=[hour_tmp2,hour_tmp1]
+
     return year_res,month_res,day_res,hour_res
     
 if __name__=="__main__":
